57ab0bf892c6d19388ddb4ff5c5f03ea5749ede6
321grga <321grga@github.com>
2026-04-27 21:02:43 +0200

    feat: dodano GuildPresences intent

--- bot.js
+++ bot.js
@@ -11,7 +11,8 @@ const client = new Client({
         GatewayIntentBits.MessageContent,
         GatewayIntentBits.GuildMessageReactions,
         GatewayIntentBits.GuildModeration,
-        GatewayIntentBits.GuildVoiceStates
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.GuildPresences
     ],
     partials: [Partials.Message, Partials.Channel, Partials.Reaction]
 });
